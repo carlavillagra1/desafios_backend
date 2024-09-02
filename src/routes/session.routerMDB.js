ec561b72e9ddab59d0c68e85b7b8754d93767069
Carla Villagra <villagraconce06@gmail.com>
2024-09-02 15:04:28 -0300

    verificacion de logeo con github

--- src/routes/session.routerMDB.js
+++ src/routes/session.routerMDB.js
@@ -10,8 +10,8 @@ router.post('/login', passport.authenticate('login', { failureRedirect: 'faillog
 router.get('/faillogin', userController.failLogin);
 router.post('/logout', userController.logout);
 router.post('/changepassword', userController.changePassword);
-router.get('/github', userController.github);
-router.get('/githubcallback', passport.authenticate('github', { failureRedirect: '/login' }), userController.githubCallback);
+router.get('/github', passport.authenticate('github', { scope: ['user:email'] }));
+router.get('/githubcallback', passport.authenticate('github', { failureRedirect: '/api/views/login' }), userController.githubCallback);
 router.put('/premium/:id', userController.changeRoles)
 // Rutas de restablecimiento de contraseña
 router.post('/reestablecerPassword', userController.requestPasswordReset); // Aquí se enviará el email con el enlace
